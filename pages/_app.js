a69d7e43ac37be9c8b66ccb81ef170c5c15c7192
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-06-01 12:18:41 -0300

    add pages component

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles.css';
+
+function MyApp({ Component, pageProps }) {
+  // Aquí puedes agregar cualquier lógica adicional para tu aplicación
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import ChatPage from '../src/presentation/pages/ChatPage';
+
+
+const Home = () => {
+  return (
+    <div>
+      <ChatPage />
+    </div>
+  );
+};
+
+export default Home;
