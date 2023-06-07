dd03691a6e432407a9c528a72599fd2471260d89
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-06-06 22:44:52 -0300

    new fetch

--- Front/src/infrasctucture/chat/adapters/ChatBotAPI.js
+++ Front/src/infrasctucture/chat/adapters/ChatBotAPI.js
@@ -8,11 +8,11 @@ export default class ChatbotAPI extends ChatRepository {
 
   async sendMessage(message) {
     try {
-      const response = await fetch('http://127.0.0.1:5050/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+      const response = await fetch('https://chatbotdefault-production.up.railway.app/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
         body: JSON.stringify({ message })
       });
 
